6587c5e3f34e3252b48b019e24e6da86758949c0
breno santos <breno.angelim@hotmail.com>
2022-01-18 14:33:35 -0300

    feat: API mock para forum de jogos antigos

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const router = jsonServer.router("db.json");
 app.db = router.db;
 
 const rules = auth.rewriter({
-  users: 600,
+  users: 644,
+  games: 444,
+  forum: 664,
 });
 
 app.use(cors());
